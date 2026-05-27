73cd0e67209b36d10b8172fa0f11c6ccadbd55ad
李 <miauperuvian@gmail.com>
2026-05-26 22:03:40 -0500

    feat(shared): add iunitofwork contract for database commits.

---
+++ ArcadiaDevs.Viora.Platform/Shared/Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace ArcadiaDevs.Viora.Platform.Shared.Domain.Repositories;
+/// <summary>
+///     Unit of work interface
+/// </summary>
+/// <remarks>
+///     This interface defines the basic operations for a unit of work
+/// </remarks>
+public class IUnitOfWork
+{
+    /// <summary>
+    ///     Commit changes to the database
+    /// </summary>
+    Task CompleteAsync(CancellationToken cancellationToken = default);
+}
