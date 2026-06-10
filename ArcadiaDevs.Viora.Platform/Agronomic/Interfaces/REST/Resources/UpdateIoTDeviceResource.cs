9bfe33cd3e1fcde57a4a21868b25baa11a2fc4b8
李 <miauperuvian@gmail.com>
2026-06-10 16:08:33 -0500

    feat(agronomic): add updateiotdeviceresource record for patch request body.

---
+++ ArcadiaDevs.Viora.Platform/Agronomic/Interfaces/REST/Resources/UpdateIoTDeviceResource.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArcadiaDevs.Viora.Platform.Agronomic.Interfaces.Rest.Resources;
+
+/// <summary>
+///     PATCH request body for updating an IoT device.
+/// </summary>
+/// <param name="DeviceName">The new device name.</param>
+/// <param name="IotDeviceStatus">The new operational status.</param>
+public record UpdateIoTDeviceResource(
+    [Required(AllowEmptyStrings = false)] string DeviceName,
+    [Required(AllowEmptyStrings = false)] string IotDeviceStatus
+);
