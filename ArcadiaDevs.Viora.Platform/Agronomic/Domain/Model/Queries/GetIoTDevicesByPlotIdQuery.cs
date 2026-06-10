b541392baa436ff5828c92e49718f110be857e67
李 <miauperuvian@gmail.com>
2026-06-10 03:38:12 -0500

    feat(agronomic): add getiotdevicesbyplotidquery for querying iot devices by plot id.

---
+++ ArcadiaDevs.Viora.Platform/Agronomic/Domain/Model/Queries/GetIoTDevicesByPlotIdQuery.cs
@@ -0,0 +1,19 @@
+﻿namespace ArcadiaDevs.Viora.Platform.Agronomic.Domain.Model.Queries;
+
+public record GetIoTDevicesByPlotIdQuery
+{
+    public int PlotId { get; init; }
+    public int AuthenticatedUserId { get; init; }
+
+    public GetIoTDevicesByPlotIdQuery(int plotId, int authenticatedUserId)
+    {
+        if (plotId <= 0)
+            throw new ArgumentException("GetIoTDevicesByPlotIdQuery requires a valid PlotId.", nameof(plotId));
+
+        if (authenticatedUserId <= 0)
+            throw new ArgumentException("GetIoTDevicesByPlotIdQuery requires a valid AuthenticatedUserId.", nameof(authenticatedUserId));
+
+        PlotId = plotId;
+        AuthenticatedUserId = authenticatedUserId;
+    }
+}
