52b97af14922164df254021f21b2a33a3955a3a3
Santi2007939 <117610911+Santi2007939@users.noreply.github.com>
2026-06-17 11:20:11 -0500

    feat(surveillance): add query service and update repository for alert.

--- ArcadiaDevs.Viora.Platform/Surveillance/Infrastructure/Persistence/EntityFrameworkCore/Repositories/AlertRepository.cs
+++ ArcadiaDevs.Viora.Platform/Surveillance/Infrastructure/Persistence/EntityFrameworkCore/Repositories/AlertRepository.cs
@@ -3,6 +3,8 @@ using ArcadiaDevs.Viora.Platform.Shared.Infrastructure.Persistence.EFC.Repositor
 using ArcadiaDevs.Viora.Platform.Surveillance.Domain.Model.Aggregates;
 using ArcadiaDevs.Viora.Platform.Surveillance.Domain.Repositories;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace ArcadiaDevs.Viora.Platform.Surveillance.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
 
 /// <summary>
@@ -11,4 +13,13 @@ namespace ArcadiaDevs.Viora.Platform.Surveillance.Infrastructure.Persistence.Ent
 public class AlertRepository(AppDbContext context)
     : BaseRepository<Alert>(context), IAlertRepository
 {
+    public async Task<IEnumerable<Alert>> FindByPlotIdInOrderByCreatedAtDescAsync(IEnumerable<long> plotIds, int limit, CancellationToken cancellationToken = default)
+    {
+        var plotIdsList = plotIds.ToList();
+        return await Context.Set<Alert>()
+            .Where(a => plotIdsList.Contains(a.PlotId.Value))
+            .OrderByDescending(a => a.CreatedAt)
+            .Take(limit)
+            .ToListAsync(cancellationToken);
+    }
 }
