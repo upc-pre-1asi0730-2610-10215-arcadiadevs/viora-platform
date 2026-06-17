9c6808a8b7e1060f7d055b3590ec111e9d72c18f
Santi2007939 <117610911+Santi2007939@users.noreply.github.com>
2026-06-16 21:11:11 -0500

    feat(surveillance): add interface for external agronomic and implementation.

--- ArcadiaDevs.Viora.Platform/Surveillance/Infrastructure/OutboundServices/Acl/IExternalAgronomicService.cs
+++
@@ -1,6 +0,0 @@
-namespace ArcadiaDevs.Viora.Platform.Surveillance.Infrastructure.OutboundServices.Acl;
-
-public interface IExternalAgronomicService
-{
-    Task<double?> FetchCurrentNdviByPlotIdAsync(long plotId, long reporterUserId, CancellationToken cancellationToken = default);
-}
