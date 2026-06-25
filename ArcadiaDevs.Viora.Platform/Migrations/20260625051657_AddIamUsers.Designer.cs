0e3029dae7cf261b72bc51b4b2acf5924fe284ef
Jahat Trinidad <jahatjassiel@gmail.com>
2026-06-25 00:17:31 -0500

    feat(iam): generate AddIamUsers migration and design-time factory

--- ArcadiaDevs.Viora.Platform/Migrations/20260625051657_AddIamUsers.Designer.cs
+++ ArcadiaDevs.Viora.Platform/Migrations/20260625051657_AddIamUsers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ArcadiaDevs.Viora.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ArcadiaDevs.Viora.Platform.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260625051657_AddIamUsers")]
+    partial class AddIamUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
