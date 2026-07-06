3dcd5c9c2fef8537036d2496e13cab37b231bedb
李 <miauperuvian@gmail.com>
2026-07-06 00:49:58 -0500

    feat(billing): add referral-code rest endpoint and di wiring.

---
+++ ArcadiaDevs.Viora.Platform/Billing/Interfaces/Rest/Resources/ReferralCodeResource.cs
@@ -0,0 +1,6 @@
+﻿namespace ArcadiaDevs.Viora.Platform.Billing.Interfaces.Rest.Resources;
+
+/// <summary>
+///     Referral code resource (REQ-REF-2..4).
+/// </summary>
+public record ReferralCodeResource(int Id, int UserId, string Code, int RewardPercent);
